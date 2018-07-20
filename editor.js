19e0409fe117a977539ceebbe819b31e8f45c845
Dylan Audic <dylan.audic@laposte.net>
2018-07-20 17:48:32 +0200

    Add menu

--- editor.js
+++ editor.js
@@ -18,14 +18,17 @@ function syntaxHighlights() {
     for (var i = 0; i < codes.length; i++) {
         var data = codes[i].textContent;
         for (var attr in obj) {
-            var regex = new RegExp(obj[attr].re, 'gi');            
+            var regex = new RegExp(obj[attr].re, 'gi');
             /*var text = regex.exec(data);*/            
             var text = data.match(regex);
             if (text) {
                 for (var n = 0; n < text.length; n++) {
                     
-                    console.log(data.match(regex));
-                    data = data.replace(regex, colorer(text[0], obj[attr].color));
+                    data.match(regex).forEach(function(e) {                        
+                        data = data.replace(e, colorer(text[0], obj[attr].color));
+                        console.log(text[n]);
+                    });
+                    
                 }   
             }                     
             codes[i].innerHTML = data;
@@ -45,6 +48,10 @@ var obj = {
     number2: {
         re: '[0-9]+,',
         color: '#85a300'
+    },
+    variable: {
+        re: 'var [A-Za-z0-9]+ ',
+        color: '#b77fdb'
     },
     new: {
         re: 'new',
@@ -81,3 +88,18 @@ window.addEventListener('load', syntaxHighlights);
 /*var colorationIntervalID = window.setInterval(function() {
     document.getElementById('code').innerHTML = replaceAll(document.getElementById('code').textContent);
 }, 2000);*/
+
+function uniq(a) {
+    var seen = {};
+    var out = [];
+    var len = a.length;
+    var j = 0;
+    for(var i = 0; i < len; i++) {
+         var item = a[i];
+         if(seen[item] !== 1) {
+               seen[item] = 1;
+               out[j++] = item;
+         }
+    }
+    return out;
+}
